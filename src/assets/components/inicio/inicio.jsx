9e2a10e53beead72e257b2b137af19e5697489ab
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-12-31 16:07:10 -0300

    Terminamos seccion de certificados y estilizamos el footer

--- src/assets/components/inicio/inicio.jsx
+++ src/assets/components/inicio/inicio.jsx
@@ -5,12 +5,16 @@ import { Link } from 'react-router-dom';
 
 export default function Inicio (){
     return(
+        
         <div>  
             <img  className="rostro" src={image}/>
-        <h1>Desarrollador Web Full Stack Freelance y Creador de Contenido</h1>
-        <p>¡Hola! Me llamo Ignacio. Soy creador de experiencias digitales sorprendentes y fáciles de usar.</p>
-        <p> Con experiencia en el ecosistema de React y Node tanto en front end como en back end, he desarrollado aplicaciones web  desde 2022.</p> 
-        <p>He trabajado como freelancer hace un año, donde  aprendì a fusionar creatividad y funcionalidad.</p>
+        <h1 className="titulo">Desarrollador Web Full Stack Freelance y Creador de Contenido</h1>
+        <div className="container">
+
+        <p className="text">¡Hola! Me llamo Ignacio. Soy creador de experiencias digitales sorprendentes y fáciles de usar.</p>
+        <p className="text"> Con experiencia en el ecosistema de React y Node tanto en front end como en back end, he desarrollado aplicaciones web  desde 2022.</p> 
+        <p className="text">He trabajado como freelancer hace un año, donde  aprendì a fusionar creatividad y funcionalidad.</p>
+        </div>
        </div>
     );
 }
